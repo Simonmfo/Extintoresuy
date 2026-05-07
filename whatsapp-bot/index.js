9d04676d6d46cdc7e6ec88e5671a252c9f42cb70
Simon Mfo <simonfleitas99@gmail.com>
2026-05-07 15:17:57 -0300

    feat: optimize for Back4App Containers compatibility

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -8,7 +8,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Express setup to keep the service alive
 let latestQR = null;
